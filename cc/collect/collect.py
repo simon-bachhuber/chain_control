1b4ec647ab02fd7958c3bac225008b082d3bf1d9
Simon Bachhuber <simon.bachhuberr@gmail.com>
2022-11-05 23:51:06 +0100

    `check_infos` and `ts` explicitly is no longer req

--- cc/collect/collect.py
+++ cc/collect/collect.py
@@ -8,17 +8,9 @@ from ..abstract import AbstractController, AbstractModel
 from ..buffer import ReplaySample, make_episodic_buffer_adder_iterator
 from ..config import use_tqdm
 from ..controller import FeedforwardController
-from ..env import ModelBasedEnv
-from ..env.wrappers.add_source import AddReferenceObservationWrapper
-from ..env.wrappers.delay import DelayActionWrapper
+from ..env.wrappers import AddRefSignalRewardFnWrapper, ReplacePhysicsByModelWrapper
 from ..types import *
-from ..utils import (
-    extract_timelimit_timestep_from_env,
-    to_jax,
-    to_numpy,
-    tree_concat,
-    tree_shape,
-)
+from ..utils import to_jax, to_numpy, tree_concat, tree_shape
 from .actor import PolicyActor
 from .source import (
     ObservationReferenceSource,
@@ -32,22 +24,14 @@ def concat_iterators(*iterators) -> ReplaySample:
     return tree_concat([next(iterator) for iterator in iterators], True)
 
 
-def _checks_and_infos(env):
-    assert not isinstance(env, ModelBasedEnv)
-    time_limit, control_timestep, ts = extract_timelimit_timestep_from_env(env)
-    return time_limit, control_timestep, ts
-
-
 @beartype
 def collect_sample(
     env: dm_env.Environment, seeds_gp: list[int], seeds_cos: list[Union[int, float]]
 ) -> ReplaySample:
 
-    _, _, ts = _checks_and_infos(env)
-
-    _, sample_gp = collect_feedforward_and_make_source(env, ts, seeds=seeds_gp)
+    _, sample_gp = collect_feedforward_and_make_source(env, seeds=seeds_gp)
     _, sample_cos = collect_feedforward_and_make_source(
-        env, ts, draw_u_from_cosines, seeds=seeds_cos
+        env, draw_u_from_cosines, seeds=seeds_cos
     )
 
     return tree_concat([sample_gp, sample_cos], True)
@@ -61,9 +45,7 @@ def collect_reference_source(
     constant_after_T: float = 3.0,
 ):
 
-    _, _, ts = _checks_and_infos(env)
-
-    source, _ = collect_feedforward_and_make_source(env, ts, seeds=seeds)
+    source, _ = collect_feedforward_and_make_source(env, seeds=seeds)
 
     if constant_after:
         source = constant_after_transform_source(source, constant_after_T)
@@ -79,22 +61,11 @@ def collect_exhaust_source(
     model: AbstractModel = None,
 ) -> ReplaySample:
 
-    time_limit, control_timestep, ts = _checks_and_infos(env)
-
     if model:
-
-        # grab delay of Mujoco Env before overwriting
-        delay = env.delay
-
-        env = ModelBasedEnv(
-            env, model, time_limit=time_limit, control_timestep=control_timestep
-        )
-
-        if delay > 0:
-            env = DelayActionWrapper(env, delay)
+        env = ReplacePhysicsByModelWrapper(env, model)
 
     # wrap env with source
-    env = AddReferenceObservationWrapper(env, source)
+    env = AddRefSignalRewardFnWrapper(env, source)
 
     N = tree_shape(source._yss)
     # collect performance of controller in environment
@@ -102,7 +73,7 @@ def collect_exhaust_source(
     iterators = []
     for i_actor in pbar:
         source.change_reference_of_actor(i_actor)
-        iterator = collect(env, controller, ts)
+        iterator = collect(env, controller)
         iterators.append(iterator)
 
     # concat samples
@@ -111,13 +82,13 @@ def collect_exhaust_source(
     return sample
 
 
-def collect(env: dm_env.Environment, controller: AbstractController, ts: jnp.ndarray):
+def collect(env: dm_env.Environment, controller: AbstractController):
 
     env.reset()
 
     _, adder, iterator = make_episodic_buffer_adder_iterator(
         bs=1,
-        ts=ts,
+        ts=env.ts,
         env_specs=make_environment_spec(env),
         actor_id=1,
     )
@@ -130,18 +101,19 @@ def collect(env: dm_env.Environment, controller: AbstractController, ts: jnp.nda
 
 def collect_feedforward_and_make_source(
     env: dm_env.Environment,
-    ts: jnp.ndarray,
     draw_fn=draw_u_from_gaussian_process,
     seeds: list[int] = [
         0,
     ],
 ) -> Tuple[ObservationReferenceSource, ReplaySample]:
 
+    ts = env.ts
+
     iterators = []
     for seed in seeds:
         us: TimeSeriesOfAct = to_jax(draw_fn(to_numpy(ts), seed=seed))
         policy = FeedforwardController(us)
-        iterator = collect(env, policy, ts)
+        iterator = collect(env, policy)
         iterators.append(iterator)
 
     sample = concat_iterators(*iterators)
