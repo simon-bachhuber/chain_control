80d237a572a9e20cb2c9bb1839abe1a0c5ecd8f2
Simon Bachhuber <simon.bachhuberr@gmail.com>
2022-11-05 17:30:19 +0100

    renames `MetaDataWrapper`

---
+++ cc/env/wrappers/attribute.py
@@ -0,0 +1,16 @@
+import dm_env
+from acme.wrappers import EnvironmentWrapper
+
+
+class AttributeWrapper(EnvironmentWrapper):
+    def __init__(self, env: dm_env.Environment, safe=True, **attrs):
+        for key, value in attrs.items():
+            if hasattr(env, key):
+                if safe:
+                    raise Exception(
+                        f"The environment {env} to be wrapped \
+                        already has an attribute `{key}`. This will make this value \
+                            unreachable."
+                    )
+            setattr(self, key, value)
+        super().__init__(env)
