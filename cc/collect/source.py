5d1ebd4579d403a4914463d1b0c5ef057f71f85f
Simon Bachhuber <simon.bachhuberr@gmail.com>
2022-11-05 23:52:19 +0100

    black+isort

--- cc/collect/source.py
+++ cc/collect/source.py
@@ -8,25 +8,26 @@ from sklearn.gaussian_process.kernels import RBF
 
 from ..abstract import AbstractObservationReferenceSource
 from ..types import *
-from ..utils import idx_in_pytree, to_numpy
+from ..utils import to_numpy, tree_slice
 
 
 class ObservationReferenceSource(AbstractObservationReferenceSource):
-    def __init__(self, 
-            ts: jnp.ndarray, 
-            yss: BatchedTimeSeriesOfRef, 
-            uss: Optional[BatchedTimeSeriesOfAct] = None, 
-            i_actor=0, 
-            reference_transform = lambda arr: arr
-        ):
+    def __init__(
+        self,
+        ts: jnp.ndarray,
+        yss: BatchedTimeSeriesOfRef,
+        uss: Optional[BatchedTimeSeriesOfAct] = None,
+        i_actor=0,
+        reference_transform=lambda arr: arr,
+    ):
         self._i_actor = i_actor
-        self._ts = ts 
-        self._yss = yss 
-        self._uss = uss 
+        self._ts = ts
+        self._yss = yss
+        self._uss = uss
         self._reference_transform = reference_transform
 
     def set_reference_transform(self, transform):
-        self._reference_transform = transform 
+        self._reference_transform = transform
 
     def get_references_for_optimisation_untransformed(self) -> BatchedTimeSeriesOfRef:
         transform = lambda arr: jnp.atleast_3d(arr)
@@ -37,42 +38,47 @@ class ObservationReferenceSource(AbstractObservationReferenceSource):
         return jax.tree_util.tree_map(transform, self._yss)
 
     def get_reference_actor(self) -> TimeSeriesOfRef:
-        return idx_in_pytree(self.get_references_for_optimisation(), start=self._i_actor)
+        return tree_slice(self.get_references_for_optimisation(), start=self._i_actor)
 
     def change_reference_of_actor(self, i_actor: int) -> None:
         self._i_actor = i_actor
-        
 
-default_kernel = 0.2*RBF(1.5)
+
+default_kernel = 0.2 * RBF(1.5)
+
+
 def draw_u_from_gaussian_process(ts, kernel=default_kernel, seed=1):
-    ts = ts[:,np.newaxis]
+    ts = ts[:, np.newaxis]
     us = GaussianProcessRegressor(kernel=kernel).sample_y(ts, random_state=seed)
     us = (us - np.mean(us)) / np.std(us)
-    return us.astype(np.float32) 
+    return us.astype(np.float32)
 
 
 def draw_u_from_cosines(ts, seed):
-    freq = seed 
+    freq = seed
     ts = ts / ts[-1]
-    omega = 2*np.pi*freq
-    return np.cos(omega*ts)[:, None]*np.sqrt(freq)
+    omega = 2 * np.pi * freq
+    return np.cos(omega * ts)[:, None] * np.sqrt(freq)
 
 
 def _constant_after_transform(refs: TimeSeriesOfRef, idx, new_ts):
-    N = len(new_ts)+1
+    N = len(new_ts) + 1
     refs_out = jnp.zeros((N, refs.shape[1]))
     refs_out = refs_out.at[:idx].set(refs[:idx])
     refs_out = refs_out.at[idx:].set(refs[idx])
-    return refs_out 
+    return refs_out
 
 
-def constant_after_transform_source(source: ObservationReferenceSource, after_T: float, new_ts: jnp.ndarray = None) -> ObservationReferenceSource:
+def constant_after_transform_source(
+    source: ObservationReferenceSource, after_T: float, new_ts: jnp.ndarray = None
+) -> ObservationReferenceSource:
     if new_ts is None:
-        new_ts = source._ts 
+        new_ts = source._ts
 
     source = copy.deepcopy(source)
     idx = jnp.where(new_ts == after_T)[0][0]
     transform = lambda arr: _constant_after_transform(arr, idx, new_ts)
     source.set_reference_transform(transform)
-    return ObservationReferenceSource(new_ts, to_numpy(source.get_references_for_optimisation()))
-
+    return ObservationReferenceSource(
+        new_ts, to_numpy(source.get_references_for_optimisation())
+    )
