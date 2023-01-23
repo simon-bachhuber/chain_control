3661ddc8735fe5f3dff95584e7b4ba216c289b87
Tobias Gessler <gessler.tobi@gmail.com>
2023-01-23 19:42:44 +0100

    feat: Update register to use generic environment creation
    Co-authored-by: stormofice <58337328+stormofice@users.noreply.github.com>

--- cc/env/register.py
+++ cc/env/register.py
@@ -1,11 +1,17 @@
 from dataclasses import dataclass
-from typing import Callable
+from typing import Callable, List
 
 from dm_control import mujoco
 from dm_control.rl import control
 
-from .envs import two_segments_v1, two_segments_v2, two_segments_v3
+from .envs import two_segments
+from .envs.two_segments import CartParams, JointParams, SegmentTask
 
+def load_physics_wrapper(cart_params: List[CartParams]):
+    def load_physics():
+        return two_segments.load_physics(cart_params)
+
+    return load_physics
 
 @dataclass
 class EnvRegisterValue:
@@ -17,12 +23,54 @@ class EnvRegisterValue:
 
 _register = {
     "two_segments_v1": EnvRegisterValue(
-        two_segments_v1.load_physics, two_segments_v1.SegmentTask, 10.0
+        load_physics_wrapper(
+            [CartParams(
+                name="cart",
+                slider_joint_params=JointParams(
+                    damping=1e-3
+                ),
+                hinge_joint_params=JointParams(
+                    damping=1e-1, springref=0, stiffness=10
+                ),
+            )]
+        ),
+        SegmentTask,
+        10.0
     ),
     "two_segments_v2": EnvRegisterValue(
-        two_segments_v2.load_physics, two_segments_v2.SegmentTask, 10.0
+        load_physics_wrapper(
+            [CartParams(
+                name="cart",
+                slider_joint_params=JointParams(
+                    damping=1e-3
+                ),
+                hinge_joint_params=JointParams(
+                    damping=3e-2, springref=0, stiffness=3
+                ),
+            )]
+        ),
+        SegmentTask,
+        10.0
     ),
     "two_segments_v3": EnvRegisterValue(
-        two_segments_v3.load_physics, two_segments_v3.SegmentTask, 10.0
+        load_physics_wrapper(
+            [CartParams(
+                name="cart",
+                slider_joint_params=JointParams(
+                    damping=1, stiffness=1
+                ),
+                hinge_joint_params=JointParams(
+                    damping=3e-2, springref=0, stiffness=2
+                ),
+            )]
+        ),
+        SegmentTask,
+        10.0
     ),
 }
+
+def register_new_env(id: str, value: EnvRegisterValue):
+    _register[id] = value
+
+def register_new_two_segment_env(id: str, cart_params: CartParams, time_limit: float = 10.0):
+    _register[id] = EnvRegisterValue(id, load_physics_wrapper(cart_params), SegmentTask, time_limit)
