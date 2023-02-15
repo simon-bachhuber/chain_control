6b47b4da7f57369a99d75596acbfbca38583c466
Simon Bachhuber <simon.bachhuberr@gmail.com>
2023-02-16 00:24:36 +0100

    ran `run_tools.sh`

--- scripts/run_tools.sh
+++ scripts/run_tools.sh
@@ -13,10 +13,11 @@ coverage run -m pytest
 rm icons/coverage.svg
 coverage-badge -o icons/coverage.svg
 
-nbtb run --inplace docs/1_defining_an_environment.ipynb 
-nbtb run --inplace docs/2_collecting_data_from_an_environment.ipynb 
-nbtb run --inplace docs/3_training_a_model.ipynb 
-nbtb run --inplace docs/4_training_a_model_and_controller.ipynb 
-nbtb run --inplace docs/5_save_loading_model_controller.ipynb 
+cd docs
+nbtb run --inplace 1_defining_an_environment.ipynb 
+nbtb run --inplace 2_collecting_data_from_an_environment.ipynb 
+nbtb run --inplace 3_training_a_model.ipynb 
+nbtb run --inplace 4_training_a_model_and_controller.ipynb 
+nbtb run --inplace 5_save_loading_model_controller.ipynb 
 
 rm train_sample.pkl model.eqx controller.eqx replay_sample.pkl
