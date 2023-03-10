91203f8ad065a78c4e102b69b23f96e59984f7ff
Simon Bachhuber <simon.bachhuberr@gmail.com>
2023-03-10 09:51:44 +0100

    allow periodic reseting of controller

--- cc/utils/visual/viewer.py
+++ cc/utils/visual/viewer.py
@@ -7,16 +7,16 @@ from dm_control.viewer import launch
 from ...env.collect import ModuleActor
 
 
-def launch_viewer_controller(env, controller, jit=True):
+def launch_viewer_controller(env, controller, jit=True, reset_every: int = -1):
     actor = ModuleActor(env.action_spec(), controller=controller, jit=jit)
-    return launch_viewer(env, actor)
+    return launch_viewer(env, actor, reset_every)
 
 
-def launch_viewer(env: dm_env.Environment, actor: core.Actor):
-    return launch(env, _policy_for_viewer(actor))
+def launch_viewer(env: dm_env.Environment, actor: core.Actor, reset_every: int = -1):
+    return launch(env, _policy_for_viewer(actor, reset_every))
 
 
-def _policy_for_viewer(actor: core.Actor):
+def _policy_for_viewer(actor: core.Actor, reset_every: int):
     actor = copy.deepcopy(actor)
     if actor._adder:
         actor._adder = None
@@ -24,6 +24,7 @@ def _policy_for_viewer(actor: core.Actor):
     class _policy:
         observe_first = True
         last_action = None
+        n_call_calls = 0
 
         def __call__(self, ts):
             if self.observe_first:
@@ -36,9 +37,15 @@ def _policy_for_viewer(actor: core.Actor):
             action = actor.select_action(ts.observation)
             self.last_action = action
 
+            self.n_call_calls += 1
+            if reset_every > 0:
+                if (self.n_call_calls % reset_every) == 0:
+                    self.reset()
+
             return action
 
         def reset(self):
+            # this flag triggers an actor reset at next __call__
             self.observe_first = True
 
     return _policy()
