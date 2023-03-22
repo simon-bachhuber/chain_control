500d7c5a21aa3b59a4bb0e01e7c60a37eef09d12
Simon Bachhuber <simon@aibe-bachhuber2.aibe.uni-erlangen.de>
2023-03-22 13:36:03 +0100

    remove acme as dependency

--- setup.py
+++ setup.py
@@ -41,8 +41,10 @@ setuptools.setup(
         # after installation run `pip list`
         # if dm_control==0.4345734 version, then
         # `pip install --upgrade dm_control`
-        "dm_acme[jax]",
-        "dm_acme[envs]",
+        # "dm_acme[jax]",
+        # "dm_acme[envs]",
+        "flax",
+        "dm_control",
         "cloudpickle",
         "optax",
         "scikit-learn",
